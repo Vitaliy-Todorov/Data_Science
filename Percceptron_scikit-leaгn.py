b9fb460a94f6aa0d9667145fd2f2ee035f6d8ef4
Vitaliy-Todorov <65270620+Vitaliy-Todorov@users.noreply.github.com>
2020-08-31 06:02:33 +0300

    Достроил график визуализации классификации

--- Percceptron_scikit-leaгn.py
+++ Percceptron_scikit-leaгn.py
@@ -59,30 +59,43 @@ print('Bepнocть: %.2f ' %accuracy_score(y_test, y_pred))
 
 from matplotlib.colors import ListedColormap
 
-def plot_decision_regions(X, y, classifier, test_idx = None, esolution = .02) :
+def plot_decision_regions(X, y, classifier, test_idx = None, resolution = .02) :
     markers = ('s', 'x', 'o', '^', 'v ')
-    colors = ('red', 'Ьlue', 'lightgreeп' , 'gray', 'суап')
-    cmap = ListedColormap(colors[:list(np.unique(y))])
+    colors = ('red', 'blue', 'lightgreen' , 'gray', 'cyan')
+    cmap = ListedColormap(colors[:len(np.unique(y))])
 
     #Определяем поверхность
     x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
     x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
     xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution), np.arange(x2_min, x2_max, resolution))
-    Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()].T))
+    Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
     Z = Z.reshape(xx1.shape)
 
     #Строим график
-    plt.contourf(xx1, xx2, X, alpha = .4, cmap = cmap)
+    plt.contourf(xx1, xx2, Z, alpha = .4, cmap = cmap)
     plt.xlim(xx1.min(), xx1.max())
     plt.ylim(xx2.min(), xx2.max())
 
     #Добавляем точки
     for idx, cl in enumerate(np.unique(y)) :
-        plt.scatter(x = X[y == cl, 0], y = X[y == cl, 1], 
-                alpha = .8, color = cmap(idx), marker = markers[idx], label = cl)
+        plt.scatter(x = X[y == cl, 0], y = X[y == cl, 1], color = cmap(idx), edgecolors = 'black',
+                alpha = .8, marker = markers[idx], label = cl, linewidths = .2, s = 17)
 
         # показать все образцы
         if test_idx :
             X_test, y_test = X[test_idx, :], y[test_idx]
-            plt.scatter(X_test[:, 0], X_test[: 1], c = '', alpha = 1,
-                    linewidths = 1, marker = 'o', s = 55, label = 'тестовый набор')
+            plt.scatter(X_test[:, 0], X_test[:, 1], color = cmap(idx), edgecolors = 'black',
+                    alpha = 1.0, marker = 'o', label = 'тестовый набор', linewidths = 10, s = 1)
+
+# %% codecell
+import matplotlib.pyplot as plt
+
+X_combinad_std = np.vstack((X_train_std, X_test_std))
+    #np.vstack(X, X1) - объединяет все элементы из X, X1 в столбец
+y_combinad_std = np.hstack((y_train, y_test))
+    #np.hstack(X, X1) - "прилепляет" снизу X, матрицу X1
+plot_decision_regions(X = X_combinad_std, y = y_combinad_std, classifier = ppn, test_idx = range(105, 150))
+plt.xlabel('дли на ле пе стка [стандартизованная]')
+plt.ylabel('ширина лепестка [стандартизованная]')
+plt.legend(loc = 'upper left')
+plt.show()
