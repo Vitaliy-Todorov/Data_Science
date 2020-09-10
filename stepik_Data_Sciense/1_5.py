9fd614822366aead8f0f7f25b8c5f9e07f584be7
Vitaliy-Todorov <65270620+Vitaliy-Todorov@users.noreply.github.com>
2020-09-10 21:55:28 +0300

    Фильтрация данных

--- stepik_Data_Sciense/1_5.py
+++ stepik_Data_Sciense/1_5.py
@@ -1,13 +1,75 @@
 import  pandas as pd
 import numpy as np
 
-student_performance = pd.read_csv(r'C:\programming\Data_Science\working\stepik_Data_Sciense\StudentsPerformance.csv')
-student_performance
+students_performance = pd.read_csv(r'C:\programming\Data_Science\working\stepik_Data_Sciense\StudentsPerformance.csv')
+students_performance
+
+# %% codecell
+#Запросы
 
 #Показать в каких строках gender принимает значение 'female'
-student_performance.gender == 'female'
+students_performance.gender == 'female'
 
-type(student_performance.gender == 'female')
+type(students_performance.gender == 'female')
 
 #вывести стодбци со сторома, в которых gender принимает значение 'female'
-student_performance.loc[student_performance.gender == 'female', ['gender', 'writing score']]
+students_performance.loc[students_performance.gender == 'female', ['gender', 'writing score']]
+
+mean_writing_score = students_performance['writing score'].mean()
+students_performance.loc[students_performance['writing score'] > mean_writing_score]
+
+students_performance.loc[(students_performance['writing score'] > 100) & (students_performance.gender == 'female')]
+
+#Доля студентов у которых lunch имеет значение free/reduced
+(students_performance['lunch'] == 'free/reduced').mean()
+
+# %% codecell
+#Среднее, дисперсия, средне квадратичное
+
+lunch_reduced = students_performance.loc[students_performance['lunch'] == 'free/reduced']
+lunch_standard = students_performance.loc[students_performance['lunch'] == 'standard']
+# lunch_reduced_score = lunch_reduced.loc[:, ['math score', 'reading score', 'writing score']]
+#вывести средне по строкам
+# lunch_reduced_score.mean(axis=1)
+lunch_reduced.mean()
+lunch_standard.mean()
+
+lunch_reduced.var()
+lunch_standard.var()
+
+lunch_reduced.std()
+lunch_standard.std()
+
+# %% codecell
+students_performance_ = students_performance.rename(columns = {
+        'parental level of education': 'parental_level_of_education',
+        'test preparation course': 'test_preparation_course',
+        'math score': 'math_score', 'reading score': 'reading_score',
+        'writing score': 'writing_score'})
+
+students_performance_
+
+students_performance_.query('writing_score > 74')
+
+students_performance_.query("gender == 'female' & writing_score > 74")
+
+writing_score_query = 78
+students_performance_.query("writing_score > @writing_score_query")
+
+#Верно! Обратите внимание, query не применяется к колонкам, название которых содержит недопустимые символы (типа пробел, слэша). Ещё пример query, аналогичный isin() -
+variants = ["bachelor's degree", "master's degree"]
+students_performance_.query("parental_level_of_education == @variants")
+
+#DataFrame.isin([x, y]) - Возвращает Series, элементами которого являются true или false, в зависимости от того, присутствует ли в строке DataFrame одно из значений x или y. Содержится ли каждый элемент в фрейме данных в значениях.
+students_performance[student_stats['parental level of education'].isin(["bachelor's degree", "master's degree"])]
+
+#Показать название всех столбцов
+list(students_performance_)
+
+#Выбираем столбци в которых есть слово'score'
+subject_score = [i for i in list(students_performance_) if 'score' in i]
+subject_score
+students_performance_[subject_score]
+
+#Выбираем столбци в которых есть слово'score'
+students_performance_.filter(like = 'score')
